901f234639f6a22fc97c58417eb565d749426221
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-01 15:09:02 +0200

    aggiunto promt + formazione celle

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+
+var celleUtente = parseInt(prompt("Di quante celle vuoi che sia il campo ?"));
+
+//Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+
+for (var i = 1; i <= celleUtente; i++) {
+    document.getElementById("container").innerHTML += `<div class="quadrato">${i}</div>`;
+}
+
+
+//Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+
+
+
+
